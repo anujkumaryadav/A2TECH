991d34eebd69df24e6488d3f21367c6fba05db32
Sejal Khanzode <khanzodesejal@gmail.com>
2024-06-04 12:42:49 +0530

    homepage done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,123 +1,204 @@
 // Icons Import
-import { FaArrowRight } from "react-icons/fa"
-import { Link } from "react-router-dom"
 
+import { useEffect, useState } from "react"
+import { FaArrowRight } from "react-icons/fa"
+import { Link, useLocation, useNavigate, useParams } from "react-router-dom"
+
+// images
+import bghome from "../assets/Images/bghome.png"
+import enrollnow from "../assets/Images/enrollnow.png"
+import home1 from "../assets/Images/home1.png"
+import home2 from "../assets/Images/home2.png"
+import home3 from "../assets/Images/home3.png"
+import home4 from "../assets/Images/home4.png"
+import home5 from "../assets/Images/home5.png"
+import home6 from "../assets/Images/home6.png"
+import home from "../assets/Images/home.png"
+import section2 from "../assets/Images/section2.png"
 // Component Imports
 import Footer from "../components/Common/Footer"
+import CTAButton from "../components/core/HomePage/Button"
 import CodeBlocks from "../components/core/HomePage/CodeBlocks"
 import HighlightText from "../components/core/HomePage/HighlightText"
 import InstructorSection from "../components/core/HomePage/InstructorSection"
-import TimelineSection from "../components/core/HomePage/Timeline"
 import LearningLanguageSection from "../components/core/HomePage/LearningLanguageSection"
-import { useEffect, useState } from "react"
-import { useLocation, useNavigate, useParams } from "react-router-dom"
-
+import TimelineSection from "../components/core/HomePage/Timeline"
 
 function Home() {
-  // const navigate = useNavigate()
-  // const [roomId, setRoomId] = useState();
-  // const handleJoin = () => {
-  //   navigate(`/live/${roomId}`)
-  // }
-
-
   return (
     <div>
       {/* Section 1 */}
-      <div className="relative mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 text-black font-manrope">
-        <div className="w-full h-screen flex flex-col items-center pt-20">
-          <div className="flex flex-col mt-14 pt-5 text-4xl font-major-mono font-normal">
-            Welcome to A2Tech
+      <div className="relative mx-auto flex w-11/12 max-w-maxContent flex-col  gap-8 font-manrope text-black">
+        <div className="mb-0 mt-10 flex flex-row pb-0">
+          <div className="mr-16 mt-10 w-[60%] items-center justify-center p-10 pb-0 text-center">
+            <h1 className="text-[50px]">
+              Online <span className="font-bold">Learning you can access</span>{" "}
+              anywhere easily!
+            </h1>
+            <p className="">Every success journey we’ve encountered.</p>
+            <Link to={"/signup"}>
+              <div className="group mx-auto mt-10 w-fit rounded-full bg-[#FFCF59] p-1  font-bold text-black drop-shadow-[0_1.5px_rgba(255,255,255,0.25)] transition-all duration-200 hover:bg-black hover:text-white">
+                <div className="flex flex-row items-center gap-2 rounded-full px-14 py-[10px] ">
+                  <p>Enroll Now</p>
+                  <FaArrowRight />
+                </div>
+              </div>
+            </Link>
           </div>
-          
-          <div className="flex flex-col mt-10 text-4xl font-normal font-major-mono">
-            Every success journey we’ve encountered.
+
+          <div className="relative">
+            <img
+              src={bghome}
+              alt=""
+              className="absolute inset-0 h-auto object-cover"
+            />
+            <img
+              src={home}
+              alt=""
+              className="relative z-10  mt-14 h-[450px] w-[350px]"
+            />
           </div>
-
-          {/* <input type="text" placeholder="Enter RoomID" value={roomId} onChange={e=>setRoomId(e.target.value)} className="blackButton mt-2 "/>
-          <button onClick={handleJoin} className="yellowButton mt-4">Join Now</button> */}
-
-          <Link to={"/signup"}>
-            <div className="group mx-auto mt-10 w-fit rounded-full bg-[#27282C] hover:bg-black p-1 font-normal text-white drop-shadow-[0_1.5px_rgba(255,255,255,0.25)] transition-all duration-200 ">
-              <div className="flex flex-row items-center gap-2 rounded-full px-14 py-[10px] ">
-                <p>Become an Instructor</p>
-                <FaArrowRight />
-              </div>
-            </div>
-          </Link>
-        </div>
-
-    {/* Code Section 1  */}
-        <div>
-          <CodeBlocks
-            position={"lg:flex-row font-manrope"}
-            heading={
-              <div className="text-4xl font-semibold font-major-mono ">
-                Unlock your
-                <HighlightText text={"coding potential"} /> with our online
-                courses.
-              </div>
-            }
-            subheading={ 
-              "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
-            }
-            ctabtn1={{
-              btnText: "Try it Yourself",
-              link: "/signup",
-              active: true,
-            }}
-            ctabtn2={{
-              btnText: "Learn More",
-              link: "/signup",
-              active: false,
-            }}
-            codeColor={"text-caribbeangreen-900"}
-            codeblock={`<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
-            backgroundGradient={<div className="codeblock1 absolute"></div>}
-          />
-        </div>
-
-        {/* Code Section 2 */}
-        <div>
-          <CodeBlocks
-            position={"lg:flex-row-reverse"}
-            heading={
-              <div className="text-4xl font-semibold font-major-mono lg:w-[70%]">
-                Start
-                <HighlightText text={"coding in seconds"} />
-              </div>
-            }
-            subheading={
-              "Go ahead, give it a try. Our hands-on learning environment means you'll be writing real code from your very first lesson."
-            }
-            ctabtn1={{
-              btnText: "Continue Lesson",
-              link: "/signup",
-              active: true,
-            }}
-            ctabtn2={{
-              btnText: "Learn More",
-              link: "/signup",
-              active: false,
-            }}
-            codeColor={"text-black"}
-            codeblock={`import React from "react";\n import CTAButton from "./Button";\nimport TypeAnimation from "react-type";\nimport { FaArrowRight } from "react-icons/fa";\n\nconst Home = () => {\nreturn (\n<div>Home</div>\n)\n}\nexport default Home;`}
-            backgroundGradient={<div className="codeblock2 absolute"></div>}
-          />
-        </div>
-
-        <div className="mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 mb-20">
-
-          {/* Timeline Section - Section 2*/}
-          <TimelineSection className="mt-10 mb-10"/>
-
-          {/* Learning Language Section - Section 3 */}
-          <LearningLanguageSection />
-
-          {/* Instruction Section - Section 4 */}
-          <InstructorSection/>
         </div>
       </div>
+      {/* section 2 */}
+      <div className="relative">
+        <img src={section2} alt="" className="h-auto w-full" />
+      </div>
+      {/* section 3 */}
+      <div className="mx-auto flex w-11/12  max-w-maxContent flex-col items-center justify-center text-center font-manrope text-black">
+        <div className="m-4 mb-5">
+          <h1 className="text-[25px]">
+            Why we are <span className="font-bold">best from others</span>?
+          </h1>
+          <p className="">
+            Empowering Your Learning Journey with Innovation and Excellence.
+          </p>
+        </div>
+        <div className="grid grid-cols-3">
+          <div class="m-8 w-full max-w-sm rounded-lg border-2 border-richblack-200 hover:border-[#9848FF] hover:bg-[#9848FF] hover:text-white">
+            <div class="flex flex-col items-center pb-10">
+              <img
+                src={home1}
+                alt=""
+                className="mt-10 h-24 w-24 rounded-full"
+              />
+              <h5 class="text-gray-900 mb-1 text-xl font-medium">
+                Digital Platform
+              </h5>
+              <span class="text-gray-500 text-sm">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been.....
+              </span>
+            </div>
+          </div>
+          <div class="m-8 w-full max-w-sm rounded-lg border-2 border-richblack-200 hover:border-[#A7CE4A] hover:bg-[#A7CE4A] hover:text-white">
+            <div class="flex flex-col items-center pb-10">
+              <img
+                src={home2}
+                alt=""
+                className="mt-10 h-24 w-24 rounded-full"
+              />
+              <h5 class="text-gray-900 mb-1 text-xl font-medium">
+                Optimal Ideation
+              </h5>
+              <span class="text-gray-500 text-sm">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been.....
+              </span>
+            </div>
+          </div>
+          <div class="m-8 w-full max-w-sm rounded-lg border-2 border-richblack-200 hover:border-[#4D93DF] hover:bg-[#4D93DF] hover:text-white">
+            <div class="flex flex-col items-center pb-10">
+              <img
+                src={home3}
+                alt=""
+                className="mt-10 h-24 w-24 rounded-full"
+              />
+              <h5 class="text-gray-900 mb-1 text-xl font-medium">
+                Favorable Setting
+              </h5>
+              <span class="text-gray-500 text-sm">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been.....
+              </span>
+            </div>
+          </div>
+          <div class="m-8 w-full max-w-sm rounded-lg border-2 border-richblack-200 hover:border-[#FF60A8] hover:bg-[#FF60A8] hover:text-white">
+            <div class="flex flex-col items-center pb-10">
+              <img
+                src={home4}
+                alt=""
+                className="mt-10 h-24 w-24 rounded-full"
+              />
+              <h5 class="text-gray-900 mb-1 text-xl font-medium">
+                Effective Interaction
+              </h5>
+              <span class="text-gray-500 text-sm">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been.....
+              </span>
+            </div>
+          </div>
+          <div class="m-8 w-full max-w-sm rounded-lg border-2 border-richblack-200 hover:border-[#F66742] hover:bg-[#F66742] hover:text-white">
+            <div class="flex flex-col items-center pb-10">
+              <img
+                src={home5}
+                alt=""
+                className="mt-10 h-24 w-24 rounded-full"
+              />
+              <h5 class="text-gray-900 mb-1 text-xl font-medium">
+                Flexible Time
+              </h5>
+              <span class="text-gray-500 text-sm">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been.....
+              </span>
+            </div>
+          </div>
+          <div class="m-8 w-full max-w-sm rounded-lg border-2 border-richblack-200 hover:border-[#FFCF59] hover:bg-[#FFCF59] hover:text-white">
+            <div class="flex flex-col items-center pb-10">
+              <img
+                src={home6}
+                alt=""
+                className="mt-10 h-24 w-24 rounded-full"
+              />
+              <h5 class="text-gray-900 mb-1 text-xl font-medium">Reliable</h5>
+              <span class="text-gray-500 text-sm">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been.....
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* section 4 */}
+      <div className="relative">
+        <img src={enrollnow} alt="" className="h-[700px] w-full" />
+        <div className="absolute left-[50%] top-1/2 -translate-x-1/2 -translate-y-1/2 transform pl-8 text-center">
+          <div className="pl-60 flex flex-col items-end gap-4 text-black">
+            <div className="text-4xl font-semibold">
+              Become an <span className="font-bold">Instructor</span>
+            </div>
+
+            <p className="w-[100%] text-[16px] font-medium ml-20">
+              Instructors from around the world teach millions of students on
+              A2Tech. We provide the tools and skills to teach what you love.
+            </p>
+
+            <div className="w-fit">
+              <Link to={"/signup"}>
+                <div className="group mx-auto w-fit rounded-full bg-black p-1 font-bold text-white drop-shadow-[0_1.5px_rgba(255,255,255,0.25)] transition-all duration-200 hover:bg-black hover:text-white">
+                  <div className="flex flex-row items-center gap-2 rounded-full px-14 py-[10px] ">
+                    <p>Start Teaching Today</p>
+                    <FaArrowRight />
+                  </div>
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+
       {/* Footer */}
       <Footer />
     </div>
